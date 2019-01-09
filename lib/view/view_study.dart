22fbb23a3a239b13b65428a612bae127334f61b7
limh <1591054297@qq.com>
2019-01-09 12:00:34 +0800

    添加学习界面UI

--- lib/view/view_study.dart
+++ lib/view/view_study.dart
@@ -5,22 +5,114 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mcu/view/study/view_mcu.dart';
+import 'package:flutter_mcu/widget/iconfont.dart';
 
 class StudyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final btnTextStyle = TextStyle(
+        color: Theme.of(context).primaryColor,
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold);
+
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          Icons.ac_unit,
-          color: Colors.transparent,
+        appBar: AppBar(
+          leading: Icon(
+            Icons.ac_unit,
+            color: Colors.transparent,
+          ),
+          title: Text('学海无涯'),
+          centerTitle: true,
         ),
-        title: Text('51'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("学习"),
-      ),
-    );
+        body: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8.0,
+            crossAxisSpacing: 8.0,
+            children: <Widget>[
+              GestureDetector(
+                  child: Card(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                        Icon(
+                          FontIcon.lvzhou_cpu,
+                          size: 48.0,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        Container(width: 10.0, height: 10.0),
+                        Text("51单片机", style: btnTextStyle),
+                      ])),
+                  onTap: () => Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (_) {
+                        return McuPage();
+                      }))),
+              Card(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Icon(
+                      FontIcon.lvzhou_lujing_guiji,
+                      size: 48.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Container(width: 10.0, height: 10.0),
+                    Text("汇编语言", style: btnTextStyle),
+                  ])),
+              Card(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Icon(
+                      Icons.code,
+                      size: 48.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Container(width: 10.0, height: 10.0),
+                    Text("汇编指令", style: btnTextStyle),
+                  ])),
+              Card(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Icon(
+                      FontIcon.Programming,
+                      size: 48.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Container(width: 10.0, height: 10.0),
+                    Text("C语言基础", style: btnTextStyle),
+                  ])),
+              Card(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Icon(
+                      FontIcon.hanshu,
+                      size: 48.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Container(width: 10.0, height: 10.0),
+                    Text("C函数库", style: btnTextStyle),
+                  ])),
+              Card(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Icon(
+                      FontIcon.suanfa,
+                      size: 48.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Container(width: 10.0, height: 10.0),
+                    Text("经典算法", style: btnTextStyle),
+                  ])),
+            ]));
   }
 }
--- lib/widget/iconfont.dart
+++ lib/widget/iconfont.dart
@@ -4,7 +4,17 @@ import 'package:flutter/widgets.dart';
 class FontIcon {
 
     FontIcon._();
+static const IconData dianziqijian = const IconData( 0xe611, fontFamily: "FontIcon");
 static const IconData add = const IconData( 0xe656, fontFamily: "FontIcon");
+static const IconData suan = const IconData( 0xe625, fontFamily: "FontIcon");
+static const IconData zhinengsuanfa = const IconData( 0xe6c8, fontFamily: "FontIcon");
 static const IconData icon_followed = const IconData( 0xe692, fontFamily: "FontIcon");
 static const IconData icon_follow = const IconData( 0xe693, fontFamily: "FontIcon");
+static const IconData icon_test = const IconData( 0xe60e, fontFamily: "FontIcon");
+static const IconData lvzhou_lujing_guiji = const IconData( 0xe62b, fontFamily: "FontIcon");
+static const IconData lvzhou_cpu = const IconData( 0xe631, fontFamily: "FontIcon");
+static const IconData suanfa = const IconData( 0xe601, fontFamily: "FontIcon");
+static const IconData ic_img_fail = const IconData( 0xec0d, fontFamily: "FontIcon");
+static const IconData hanshu = const IconData( 0xe668, fontFamily: "FontIcon");
+static const IconData Programming = const IconData( 0xe64c, fontFamily: "FontIcon");
 }
